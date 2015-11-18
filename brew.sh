ec613d3aeccaa32c01850cd3bfaf0abdb51f7df3
Sébastien Roy <sebastien.roy@anekdotes.com>
2015-11-18 15:16:52 -0500

    new cleanup to add _notes

--- brew.sh
+++ brew.sh
@@ -4,14 +4,14 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 brew tap phinze/homebrew-cask
 brew install brew-cask
-brew install vim
+#brew install vim
 
 brew cask install alfred
 brew cask install appcleaner
 brew cask install cyberduck
 brew cask install divvy
 brew cask install firefox
-brew cask install gimp
+#brew cask install gimp
 brew cask install google-chrome
 brew cask install iterm2
 #brew cask install mamp-pro
