bf6f7416699824f0a32a4b7ac397f2a526d5b0b5
Sébastien Roy <*********@********.com>
2019-09-03 14:57:21 -0400

    cleanier and up to date version

--- brew.sh
+++ brew.sh
@@ -1,52 +1,26 @@
 #!/bin/sh
 #################################################
 
-# Setup
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap phinze/homebrew-cask
-brew install brew-cask
-#################################################
-
 # Mac Utilities
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 brew cask install alfred
-brew cask install divvy
-brew cask install itsycal
-brew cask install evernote
-# brew cask install cleanmymac
+brew cask install spectacle
 #################################################
 
 # Development
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 brew cask install iterm2
-brew cask install gitup
-brew cask install atom
-brew cask install sublime-text
-# brew cask install cyberduck
-brew cask install filezilla
-brew cask install firefox
+brew cask isntall visual-studio-code
 brew cask install google-chrome
 brew cask install postman
-# brew cask install sketch
-# brew install vim
-# brew cask install mamp-pro
-# brew cask install parallels-desktop
 #################################################
 
 # Communications
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-brew cask install ryver
 brew cask install teamviewer
-# brew cask install line
-# brew cask install franz
 #################################################
 
 # Fun
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 brew cask install spotify
-brew cask install steam
-brew cask install curse
-brew cask install battle-net
-brew cask install razer-synapse
 #################################################
--- install.sh
+++ install.sh
@@ -1,71 +1,144 @@
 #!/bin/sh
 
-code="$HOME/code"
+# Ask for the administrator password upfront
+sudo -v
 
-echo "Creating working directories ......................"
-for directory in $code "$HOME/.bin" "$code/dev" "$code/repos"; do
-  if [ ! -d $directory ]; then
-    mkdir $directory
-    echo "$directory created"
-  fi
-done
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+# Color variables
+GREEN='\033[32m'
+NC='\033[0m'
+BLUE='\033[36m'
 
-echo "Installing composer ..............................."
-rm "$HOME/.bin/composer"
-cd "$code/dev"
-curl -sS https://getcomposer.org/installer | php
-ln -s $code/dev/composer.phar $HOME/.bin/composer
-echo "Composer installed"
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+
+echo "###################################################"
+echo "#                                                 #"
+echo "#      You are about to setup this computer       #"
+echo "#                  lets do it                     #"
+echo "#                                                 #"
+echo -e "###################################################\n\n"
+
+read -p "Press enter to continue"
+
+
+echo "Setting OSX preferences ..........................."
+sleep 1
+bash ./osx.sh
+echo -e "${GREEN}Preferences setted${NC}"
+echo -e "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+
+
+echo "Installing Brew ..................................."
+sleep 1
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo -e "${GREEN}Homebrew installed${NC}"
+echo -e "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+
+
+echo "Installing Brew Cask .............................."
+sleep 1
+brew tap caskroom/cask
+echo -e "${GREEN}Homebrew installed${NC}"
+echo -e "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+
+
+echo "Installing softwares .............................."
+sleep 1
+bash ./brew.sh
+echo -e "${GREEN}Softwares installed${NC}"
+echo -e "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+
+
+echo "Installing Composer ..............................."
+sleep 1
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+echo -e "${GREEN}Composer Installed${NC}"
+echo -e "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+
+
+echo "Creating ~/Repos directories ......................"
+sleep 1
+if [ ! -d ~/Repos ]; then
+  mkdir ~/Repos
+  echo -e "${GREEN}Repos folder created${NC}"
+else
+  echo -e "${GREEN}Repos folder already exist${NC}"
+fi
+echo -e "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+
 
 echo "Installing oh-my-zsh .............................."
+sleep 1
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   curl -L http://install.ohmyz.sh | sh
   sudo chsh -s $(which zsh) $(whoami)
   rm $HOME/.zshrc
 fi
-echo "Oh-my-zsh installed"
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+echo -e "${GREEN}Oh-my-zsh installed${NC}"
+echo -e "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+
 
 echo "Creating simlink .................................."
-if [ ! -f $HOME/.env ]; then
-  touch $HOME/.env
-  echo "~/.env created"
-fi
-DOTFILES="$HOME/dotfiles/tolink"
+sleep 1
+DOTFILES=$HOME/dotfiles/tolink
 for file in $(ls $DOTFILES); do
-  if [ ! -L "$HOME/.$file" ]; then
+  if [ ! -L "${HOME}/.$file" ]; then
     ln -s $DOTFILES/$file $HOME/.$file
-    echo "$file linked"
+    echo -e "${GREEN}${file} linked${NC}"
   fi
 done
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+echo -e "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
 
 echo "Installing fonts .................................."
+sleep 1
 SOURCEFONTS="$HOME/dotfiles/resources/fonts"
 TARGETFONTS="$HOME/Library/Fonts"
 for font in $(ls $SOURCEFONTS); do
-  if [ ! -e "$TARGETFONTS/$font" ]; then
+  if [ ! -e "${TARGETFONTS}/${font}" ]; then
     cp $SOURCEFONTS/$font $TARGETFONTS/$font
-    echo "$font installed"
+    echo -e "${GREEN}${font} installed${NC}"
   fi
 done
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+echo -e "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
 
-echo "Installing all softwares .........................."
-bash ./brew.sh
-echo "Sorfwares installed"
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
 
-echo "Setting up Mac config ............................."
-bash ./osx
-echo "Config setted"
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+echo "Installing node and npm ..........................."
+sleep 1
+brew install node
+echo -e "${GREEN}Node and npm installed${NC}"
+echo -e "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
 
-echo "Cloning vim plugins ..............................."
-bash ./vim
-echo "Plugins cloned"
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
 
-echo "All done master!"
+echo "Installing gulp ..................................."
+sleep 1
+sudo shown $USER /usr/local/lib/node_modules
+npm install gulp-cli -g
+echo -e "${GREEN}Gulp installed${NC}"
+echo -e "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+
+
+echo "Creating SSH key .................................."
+sleep 1
+read -e -p "Please enter your desired SSH passphrase (leave blank for none): " sshpassphrase
+ssh-keygen -t rsa -N sshpassphrase -f temp_key
+mv temp_key ~/.ssh/id_rsa
+mv temp_key.pub ~/.ssh/id_rsa.pub
+echo -e "${GREEN}SSH created${NC}"
+echo -e "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+
+
+echo "Installing Vundle ................................."
+sleep 1
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+echo -e "${GREEN}Vundle installed${NC}"
+echo -e "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+
+
+
+echo -e "${BLUE}   _   _ _       _                                        _"
+echo "  /_\ | | |   __| | ___  _ __   ___   _ __ ___   __ _ ___| |_ ___ _ __"
+echo " //_\\\\| | |  / _\` |/ _ \| '_ \ / _ \ | '_ \` _ \ / _\` / __| __/ _ \ '__|"
+echo "/  _  \ | | | (_| | (_) | | | |  __/ | | | | | | (_| \__ \ ||  __/ |"
+echo -e "\_/ \_/_|_|  \__,_|\___/|_| |_|\___| |_| |_| |_|\__,_|___/\__\___|_|${NC}"
+
+echo "\n\n\n"
+echo "enter 'relaod' to relaod the shell"
